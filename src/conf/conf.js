8d8b0e6d3d5311151d8cdf5ea08517006debffe4
Gaurav Kumar <0304gaurav@gmail.com>
2025-07-05 15:54:56 +0530

    config.js file created in which various services are created

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,9 +1,1 @@
-const conf={
-    appwriteUrl: String(import.meta.env.VITE_APP_APPWRITE_URL),
-    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
-    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
-    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTON_ID),
-    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),     
-}
-
-export default 
+export default
